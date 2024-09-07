4b52628081ee1b2dfae00f4d002af13b49dae759
meer-khan <shahmirkhan519@gmail.com>
2024-09-07 17:16:22 +0500

    people entry exit

--- src/routers/people_enter_exit.py
+++ src/routers/people_enter_exit.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, HTTPException, status, Response,  WebSocket, WebSocketDisconnect
+from src.routers.intrusion_stats_ws import broadcast_analysis
 from pymongo.collection import Collection
+from typing_extensions import Dict, Any
 from src.schemas import data_schemas
 from db.db_models import create_models
 from icecream import ic
 import datetime
-# from
 
 router = APIRouter(tags=["entry-exit-logs"], prefix="/cameras")
-collections = create_models()
-
+collections: Dict[str, Collection] = create_models()
 
 
 # Route to add a new camera
@@ -46,48 +46,8 @@ async def log_event(log_data : data_schemas.LogEvent, response : Response):
         "cameraId": log_data.cameraId,
         "type": log_data.type
     })
+
+    # Trigger the real-time broadcast after logging the event
+    await broadcast_analysis()
     
     return {"status": "success", "message": "Event logged successfully."}
-
-# # Route to get count of people inside the building and assembly line area
-# @router.get("/current_count/")
-# async def get_current_count():
-#     # Aggregate to find the current count of people in each area
-#     pipeline = [
-#         {"$lookup": {
-#             "from": "Cameras",
-#             "localField": "camera_id",
-#             "foreignField": "camera_id",
-#             "as": "camera_info"
-#         }},
-#         {"$unwind": "$camera_info"},
-#         {"$group": {
-#             "_id": "$camera_info.location",
-#             "entries": {"$sum": {"$cond": [{"$eq": ["$event_type", "entry"]}, 1, 0]}},
-#             "exits": {"$sum": {"$cond": [{"$eq": ["$event_type", "exit"]}, 1, 0]}}
-#         }},
-#         {"$project": {
-#             "location": "$_id",
-#             "current_count": {"$subtract": ["$entries", "$exits"]}
-#         }}
-#     ]
-    
-#     result = list(entry_exit_logs.aggregate(pipeline))
-#     return {"status": "success", "data": result}
-
-# # Route to get count of people who entered and left in the last 24 hours
-# @router.get("/last_24_hours_count/")
-# async def last_24_hours_count():
-#     now = datetime.utcnow()
-#     last_24_hours = now - timedelta(hours=24)
-    
-#     pipeline = [
-#         {"$match": {"timestamp": {"$gte": last_24_hours}}},
-#         {"$group": {
-#             "_id": "$event_type",
-#             "count": {"$sum": 1}
-#         }}
-#     ]
-    
-#     result = list(entry_exit_logs.aggregate(pipeline))
-#     return {"status": "success", "data": result}
