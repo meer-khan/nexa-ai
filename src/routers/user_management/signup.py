6edb3147fc4b262b6af0058834d3376489999584
meer-khan <shahmirkhan519@gmail.com>
2024-10-11 02:21:46 +0500

    updated signup

--- src/routers/user_management/signup.py
+++ src/routers/user_management/signup.py
@@ -1,70 +1,18 @@
-from fastapi import status, APIRouter, Response, HTTPException, Form, Request
-from fastapi.responses import HTMLResponse
+from fastapi import status, APIRouter, Response, HTTPException, Form
 from icecream import ic
 from pydantic import ValidationError
-from bson import ObjectId
 import datetime
 from datetime import timedelta
 from schemas import data_schemas
 import app
-import os
-from utils import oauth
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from decouple import config
-import smtplib
-import pathlib
-from utils import password_manager
+from utils import jwt_helper
+from utils import hash_helper
 
 router = APIRouter(tags=["user-management"], prefix="/user")
 
 
-def link_generator(route: str, token: str):
-    host = config("EMAIL_HOST")
-    port = config("EMAIL_PORT")
-    return f"http://{host}:{port}/{route}/?token={token}"
 
-
-def send_email(
-    token: str, subject: str, recipient_email: str, body_message: str, route: str
-):
-    # Your email credentials
-
-    sender_email = config("EMAIL")
-    sender_password = config("EMAIL_PASSWORD")
-
-    # Create the MIME object
-    msg = MIMEMultipart()
-    msg["From"] = sender_email
-    msg["To"] = recipient_email
-    msg["Subject"] = subject
-
-    # Create body text
-    link = link_generator(route=route, token=token)
-    body_text = f"{body_message} --- {link}"
-    msg.attach(MIMEText(body_text, "plain"))
-
-    # Connect to the SMTP server and send the email
-    # with smtplib.SMTP("smtp.office365.com", 587) as server:
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
-        server.starttls()
-        server.login(sender_email, sender_password)
-        err = server.sendmail(sender_email, recipient_email, msg.as_string())
-        # TODO: ERROR LOG
-
-
-def get_template(html_file_name: str) -> str:
-    template_path = os.path.join(
-        str(pathlib.Path("__file__").resolve().parent),
-        "templates",
-        html_file_name,
-    )
-    with open(template_path, "r") as f:
-        html_content = f.read()
-    return html_content
-
-
-@router.post("/signup", status_code=status.HTTP_201_CREATED)
+@router.post("/super-admin/signup", status_code=status.HTTP_201_CREATED)
 async def signup(
     response: Response,
     userName: str = Form(...),
@@ -79,7 +27,7 @@ async def signup(
             "email": email,
             "password1": password1,
             "password2": password2,
-            "termsConditions": termsConditions,
+            "termsConditions": termsConditions
         }
         try:
             user_data = data_schemas.Signup(
@@ -101,18 +49,11 @@ async def signup(
         # If user exists, send email to the existing account
         if check_email_exists:
             if not check_email_exists.get("active"):
-                access_token, _, _ = oauth.create_access_token(
+                access_token, _, _ = jwt_helper.create_access_token(
                     {
                         "sub": str(check_email_exists.get("_id")),
                         "ver": check_email_exists.get("passVer"),
                     }
-                )
-                send_email(
-                    token=access_token,
-                    subject="Verify Account - HOMESETV",
-                    recipient_email=user_data.get("email"),
-                    body_message="Click on the given link to verify your account",
-                    route="user/verify-account",
                 )
                 response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
                 return {
@@ -127,7 +68,7 @@ async def signup(
                     "status_code": response.status_code,
                 }
 
-        user_data["password"] = password_manager.hash(user_data["password1"])
+        user_data["password"] = hash_helper.hash(user_data["password1"])
         user_data.pop("password1")
         user_data.pop("password2")
         query_dict = {"plan_id": 0}
@@ -153,18 +94,11 @@ async def signup(
         ic(inserted_id.inserted_id)
 
         # Token generation for sending email to verify the user email
-        access_token, access_jti, access_exp = oauth.create_access_token(
+        access_token, access_jti, access_exp = jwt_helper.create_access_token(
             {"sub": str(inserted_id.inserted_id), "ver": user_data.get("passVer")}
         )
 
         # SEND EMAIL TO THE USER TO VERIFY THE ACCOUNT
-        send_email(
-            token=access_token,
-            subject="Verify Account - HOMESETV",
-            recipient_email=user_data.get("email"),
-            body_message="Click on the given link to verify your account",
-            route="user/verify-account",
-        )
 
         # TODO: Log - I
         return {"detail": "user registered successfully", "status_code": response.status_code}
@@ -175,16 +109,3 @@ async def signup(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"internal server error {ex}",
         )
-
-
-@router.get(path="/verify-account/", response_class=HTMLResponse)
-def check_user_email(request: Request, response: Response):
-    token = request.query_params.get("token")
-    token_data =oauth.get_current_user(token=token)
-    update_result = app.col_user.update_one({"_id": ObjectId(token_data.get("sub"))}, {"$set": {"active": True}})
-    if update_result:
-        response.status_code = status.HTTP_200_OK
-        html_content = get_template(html_file_name="account_verified.html")
-        return HTMLResponse(content=html_content)
-    html_content = get_template(html_file_name="account_not_verified.html") 
-    return HTMLResponse(content=html_content, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
