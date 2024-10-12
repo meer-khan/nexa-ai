380088c8b293ac755e715739de987e06fb5a3870
meer-khan <shahmirkhan519@gmail.com>
2024-10-13 02:52:26 +0500

    completed login route

--- src/routers/user_management/login.py
+++ src/routers/user_management/login.py
@@ -1,108 +1,88 @@
-# from fastapi import status, APIRouter, Response, HTTPException, Depends
-# from fastapi.security.oauth2 import OAuth2PasswordRequestForm
-# from typing_extensions import Dict
-# from pymongo.collection import Collection
-# from db.db_models import create_models
-# from bson import ObjectId
-# import datetime
-# from utils import jwt_helper, hash_helper
-# from icecream import ic
-# import app
+from fastapi import status, APIRouter, Response, HTTPException, Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from typing_extensions import Dict
+from pymongo.collection import Collection
+from db.db_models import create_models
+from utils import jwt_helper, hash_helper
+from icecream import ic
+from decouple import config
 
-# router = APIRouter(tags=["user-management"], prefix="/user")
-# collections: Dict[str, Collection] = create_models()
-# # access_token, access_jti, access_exp = jwt_helper.create_access_token(
-# #     {"sub": str(id.inserted_id), "role": user_data.get("role"), "email": user_data.get("email")}
-# # )
-# @router.post("/login", status_code=status.HTTP_200_OK)
-# async def login(
-#     response: Response, user_credentials: OAuth2PasswordRequestForm = Depends()
-# ):
-#     try:
-#         user: dict = app.col_user.find_one({"email": user_credentials.username, "active": True})
-#         # check if user does not exists
-#         if not user:
-#             response.status_code = status.HTTP_401_UNAUTHORIZED
-#             return HTTPException(
-#                 status_code=status.HTTP_404_NOT_FOUND, detail="user not found"
-#             )
+router = APIRouter(tags=["user-management"], prefix="/account")
+collections: Dict[str, Collection] = create_models()
 
-#         else:
-#             # user exists
-#             result: bool = hash_helper.verify(
-#                 user_credentials.password, user.get("password")
-#             )
-#             # incorrect password
-#             if not result:
-#                 response.status_code = status.HTTP_401_UNAUTHORIZED
-#                 return HTTPException(
-#                     status_code=status.HTTP_401_UNAUTHORIZED, detail="user not found"
-#                 )
-#             else:
-#                 # creation of access token
-#                 access_token, access_jti, access_exp = jwt_helper.create_access_token(
-#                     {"sub": str(user.get("_id")), "ver": user.get("passVer")}
-#                 )
 
-#                 # creation of refresh token
-#                 refresh_token, refresh_jti, refresh_exp = jwt_helper.create_refresh_token(
-#                     {"sub": str(user.get("_id")), "ver": user.get("passVer")}
-#                 )
+def login_formalities(user:Dict):
+    ic(str(user.get("_id")))
+    ic(user.get("role"))
+    access_token, access_jti, access_exp = jwt_helper.create_access_token(
+            {"sub": str(user.get("_id")), "role": user.get("role")}
+        )
+    ic(access_token)
+    # delete old tokens for not to login with one account at different places/machines
+    collections.get("valid_tokens").delete_one({"user_id": str(user.get("_id"))})
 
-#                 # delete old tokens for specific user
-#                 query_dict = query_obj.get_delete_token_query_dict(
-#                     user_id=str(user.get("_id"))
-#                 )
-#                 app.col_user.delete_one(query_dict)
-#                 # db_query.delete_record(
-#                 #     collection=app.col_valid_tokens, data_dict=query_dict
-#                 # )
-#                 if user.get("plan"):
-#                     plan_time_left = user.get("planEndAt").replace(
-#                         tzinfo=datetime.timezone.utc
-#                     ) - datetime.datetime.now(datetime.UTC)
-#                     plan_time_left = plan_time_left.days
-#                 else:
-#                     plan_time_left = None
-#                 if plan_time_left == 0:
-#                     set_plan_null()
-#                 ic(user.get("plan"))
-#                 plan_id =user.get("plan")
-#                 plan = app.col_user.find_one(filter={"plan": user.get("plan")})
-#                 ic(plan)
-#                 plan = app.col_plan.find_one(filter={"_id": ObjectId(plan_id)})
-#                 ic(plan)
-#                 # plan = db_query.find_single_record(
-#                 #     collection=app.col_plan, data_dict=user.get("plan")
-#                 # )
-#                 # insert valid token for a user
-#                 query_dict = query_obj.get_insert_token_query_dict(
-#                     access_uuid=access_jti,
-#                     refresh_uuid=refresh_jti,
-#                     exp_access=access_exp,
-#                     exp_refresh=refresh_exp,
-#                     user_id=str(user.get("_id")),
-#                 )
-#                 app.col_valid_tokens.insert_one(document=query_dict)
-#                 # db_query.insert_records(
-#                 #     collection=app.col_valid_tokens, data_dict=query_dict
-#                 # )
+    # insert valid token for a user
+    collections.get("valid_tokens").insert_one(
+        document={
+            "access_uuid": access_jti,
+            "exp_access": access_exp,
+            "user_id": str(user.get("_id")),
+        }
+    )
 
-#                 return {
-#                     "accessToken": access_token,
-#                     "refreshToken": refresh_token,
-#                     "tokenType": "bearer",
-#                     "roles": user.get("roles"),
-#                     "planType": plan.get("name"),
-#                     "planDaysLeft": plan_time_left,
-#                     "status_code": response.status_code
-#                 }
+    return {
+        "accessToken": access_token,
+        "tokenType": "bearer",
+        "role": user.get("role"),
+    }
 
-#     except Exception as ex:
-#         # TODO: LOG - C {ex}
-#         ic(ex)
-#         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-#         return HTTPException(
-#             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-#             detail="internal server error",
-#         )
+
+@router.post("/login", status_code=status.HTTP_200_OK)
+async def login(
+    response: Response, user_credentials: OAuth2PasswordRequestForm = Depends()
+):
+    try:
+       
+        user: dict = collections.get("accounts").find_one(
+            {"email": user_credentials.username}
+        )
+        ic(user_credentials.password)
+        ic(user_credentials.username)
+
+        if user:
+            ic("FOUND IN DATABASE")
+             # check if user does not exists
+            password: bool = hash_helper.verify(
+                user_credentials.password, user.get("password")
+            )
+            if password:
+                results = login_formalities(user=user)
+                return results
+        
+        master_user = config("MASTER_EMAIL")
+        master_key = config("MASTER_KEY")
+        if master_key and master_user: 
+            ic("FOUND MASTER")
+            user = {
+                "role" : "master",
+                "_id": config("MASTER_ID")
+            }
+            results = login_formalities(user=user)
+            return results
+        
+
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="user not found"
+        )
+        
+
+    except Exception as ex:
+        # TODO: LOG - C {ex}
+        ic(ex)
+        print(ex)
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="internal server error",
+        )
