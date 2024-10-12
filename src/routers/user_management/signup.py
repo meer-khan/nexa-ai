9de44c03552f905473a3cdd7f134219346ba1890
meer-khan <shahmirkhan519@gmail.com>
2024-10-13 02:51:44 +0500

    completed signup api for super admin using master key

--- src/routers/user_management/signup.py
+++ src/routers/user_management/signup.py
@@ -1,14 +1,13 @@
-from fastapi import status, APIRouter, Response, HTTPException, Form
+from fastapi import status, APIRouter, Response, HTTPException, Form, File, UploadFile, Depends
 from icecream import ic
-from typing_extensions import Dict, List
+from typing_extensions import Dict
 from pymongo.collection import Collection
 from db.db_models import create_models
 from pydantic import ValidationError
 import datetime
+from utils.jwt_helper import verify_token, get_current_user
 from constants.constants import roles
-from schemas import data_schemas
-import app
-from utils import jwt_helper
+from src.schemas import data_schemas
 from utils import hash_helper
 
 router = APIRouter(tags=["user-management"], prefix="/account")
@@ -20,52 +19,62 @@ async def signup(
     response: Response,
     companyName: str = Form(...),
     email: str = Form(...),
-    password1: str = Form(...),
-    password2: str = Form(...),
+    password: str = Form(...),
+    confirmPassword: str = Form(...),
     termsConditions: bool = Form(...),
+    companyLogo: UploadFile = File(...),  # Company logo as file
+    token: str = Depends(get_current_user)
 ):
     try:
         user_data = {
             "userName": companyName,
             "email": email,
-            "password1": password1,
-            "password2": password2,
+            "password": password,
+            "confirmPassword": confirmPassword,
             "termsConditions": termsConditions
         }
         try:
             user_data = data_schemas.Signup(
                 companyName=companyName,
                 email=email,
-                password1=password1,
-                password2=password2,
+                password=password,
+                confirmPassword=confirmPassword,
                 termsConditions=termsConditions,
             )
             user_data = user_data.model_dump()
+            
         except ValidationError as exc_info:
             response.status_code = status.HTTP_400_BAD_REQUEST
             return HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail=exc_info.errors()
             )
-
-        check_email_exists = collections.get("accounts").find_one({"email": user_data.get("email")})
-
-        # If user exists, send email to the existing account
-        if not check_email_exists:
+        if token.get("role") != "master":
+            return HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You do not have the necessary permissions to perform this action."
+            )
+        
+        if collections.get("accounts").find_one(filter={"email": email}):
             response.status_code = status.HTTP_409_CONFLICT
-            return {
-                "detail": f"Your account against email address {email} already exists",
-                "status_code": response.status_code,
-            }
+            return HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"Company profile with given email {email} alread exists."
+            )
 
-        user_data["password"] = hash_helper.hash(user_data["password1"])
-        user_data.pop("password1")
-        user_data.pop("password2")
+        # user_data.update({"companyLogo": companyLogo})
+        # Read and store the company logo
+        logo_binary = await companyLogo.read()
+        # check_email_exists = collections.get("accounts").find_one({"email": user_data.get("email")})
+
+        user_data["password"] = hash_helper.hash(user_data.get("password"))
+        user_data.pop("confirmPassword")
         user_data.update(
             {
                 "roles":roles.super_admin ,
                 "createdAt": datetime.datetime.now(datetime.timezone.utc),
                 "updatedAt": datetime.datetime.now(datetime.timezone.utc),
-                "active": False,
+                "active": True, 
+                "companyLogo": logo_binary
             }
         )
         # Insert user data into database
@@ -73,7 +82,8 @@ async def signup(
         ic(id.inserted_id)
         # Token generation for sending email to verify the user email
         # TODO: Log - I
-        return {"detail": "user registered successfully", "status_code": response.status_code}
+        response.status_code = status.HTTP_201_CREATED
+        return {"detail": "Company profile created successfully", "status_code": response.status_code}
 
     except Exception as ex:
         # TODO: Log - C {ex}
