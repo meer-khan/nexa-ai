2ba6a800f16791d74dcafe6ba6320c69ea84a5c6
meer-khan <shahmirkhan519@gmail.com>
2024-10-13 05:19:03 +0500

    updated login

--- src/routers/user_management/login.py
+++ src/routers/user_management/login.py
@@ -3,7 +3,7 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from typing_extensions import Dict
 from pymongo.collection import Collection
 from db.db_models import create_models
-from utils import jwt_helper, hash_helper
+from utils import jwt_helper, passwords_helper
 from icecream import ic
 from decouple import config
 
@@ -11,22 +11,19 @@ router = APIRouter(tags=["user-management"], prefix="/account")
 collections: Dict[str, Collection] = create_models()
 
 
-def login_formalities(user:Dict):
-    ic(str(user.get("_id")))
-    ic(user.get("role"))
+def login_formalities(user: Dict):
     access_token, access_jti, access_exp = jwt_helper.create_access_token(
-            {"sub": str(user.get("_id")), "role": user.get("role")}
-        )
-    ic(access_token)
+        {"sub": str(user.get("_id")), "role": user.get("role")}
+    )
     # delete old tokens for not to login with one account at different places/machines
-    collections.get("valid_tokens").delete_one({"user_id": str(user.get("_id"))})
+    collections.get("valid_tokens").delete_one({"user_email": str(user.get("email"))})
 
     # insert valid token for a user
     collections.get("valid_tokens").insert_one(
         document={
             "access_uuid": access_jti,
             "exp_access": access_exp,
-            "user_id": str(user.get("_id")),
+            "user_email": str(user.get("email")),
         }
     )
 
@@ -42,45 +39,34 @@ async def login(
     response: Response, user_credentials: OAuth2PasswordRequestForm = Depends()
 ):
     try:
-       
         user: dict = collections.get("accounts").find_one(
             {"email": user_credentials.username}
         )
-        ic(user_credentials.password)
-        ic(user_credentials.username)
 
         if user:
-            ic("FOUND IN DATABASE")
-             # check if user does not exists
-            password: bool = hash_helper.verify(
+            password: bool = passwords_helper.verify(
                 user_credentials.password, user.get("password")
             )
             if password:
                 results = login_formalities(user=user)
                 return results
-        
+
         master_user = config("MASTER_EMAIL")
         master_key = config("MASTER_KEY")
-        if master_key and master_user: 
-            ic("FOUND MASTER")
-            user = {
-                "role" : "master",
-                "_id": config("MASTER_ID")
-            }
+        if master_user == user_credentials.username and passwords_helper.verify(
+            user_credentials.password, master_key
+        ):
+            ic(master_user)
+            user = {"role": "master", "_id": config("MASTER_ID"), "email": master_user}
             results = login_formalities(user=user)
             return results
-        
 
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="user not found"
         )
-        
 
     except Exception as ex:
-        # TODO: LOG - C {ex}
-        ic(ex)
-        print(ex)
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
