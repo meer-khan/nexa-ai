81a88be5510e0be6ffdaf9b841769f5e1817d397
meer-khan <shahmirkhan519@gmail.com>
2024-10-13 05:17:07 +0500

    updated constants

--- constants/constants.py
+++ constants/constants.py
@@ -2,4 +2,4 @@ from typing_extensions import LiteralString
 
 from munch import Munch
 
-roles = Munch({"super_admin" : "super-admin", "admin" : "admin", "user" : "user"})
+roles = Munch({"super_admin" : "super-admin", "admin" : "admin", "user" : "user", "master": "master"})
