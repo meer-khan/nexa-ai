94d5ef74e3fdfdee618c30a2f51209012ffb2388
meer-khan <shahmirkhan519@gmail.com>
2024-10-28 02:21:31 +0500

    updated

---
+++ utils/logging_helper.py
@@ -0,0 +1,11 @@
+# import logging
+
+# def logger():
+#     logger = logging.getLogger(__name__)
+#     # logging.basicConfig(filename='nexa-ai.log', format="{asctime} - {levelname} - {message}", datefmt="%d-%m-%Y %H:%M:%S")
+#     file_handler = logging.FileHandler("app.log", mode="a", encoding="utf-8")
+#     formatter = logging.Formatter(fmt ="{asctime} - {levelname} - {message}", datefmt="%d-%m-%Y %H:%M:%S" )
+#     file_handler.setFormatter(formatter)
+#     logger.addHandler(file_handler)
+#     return logger
+    
